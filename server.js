7426d624bc0555bf634521d2c705cfce8302b766
MarcTzh <marcustan26@hotmail.com>
2022-08-12 12:17:25 +0800

    simple server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/version', (req, res) => {
+  res.send('v1.0')
+})
+
+app.post('/slack', async (req, res) => {
+  res.sendStatus(200)
+  const axios = require('axios')
+  await axios.post('https://asa.team/api/slack/ping')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
