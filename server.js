7aea2fc0bfc905e62f1f657aba964325a3cda4eb
MarcTzh <marcustan26@hotmail.com>
2022-08-13 11:57:42 +0800

    execute axios call to slack api before res.send

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/version', (req, res) => {
 app.post('/slack', async (req, res) => {
   const start = new Date().getTime()
   functions.logger.log('Start of slack')
-  res.sendStatus(200)
-  functions.logger.log('After res send 200', new Date().getTime() - start)
   const axios = require('axios')
   await axios.post('https://asa.team/api/slack/ping')
+  res.sendStatus(200)
+  functions.logger.log('After res send 200', new Date().getTime() - start)
   functions.logger.log('End of slack', new Date().getTime() - start)
 })
 
