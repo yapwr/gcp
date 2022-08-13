f8b323172ba75fa0b265f482e1509a3eefc64cf8
MarcTzh <marcustan26@hotmail.com>
2022-08-13 17:22:58 +0800

    change version number

--- server.js
+++ server.js
@@ -7,13 +7,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v1.2')
+  res.send('v1.3 asaSlack implemented')
 })
 
+// To ping for testing
 app.post('/slack', async (req, res) => {
   res.sendStatus(200)
   const axios = require('axios')
   await axios.post('https://asa.team/api/slack/ping', req)
+})
+
+app.post('/asaSlack', async (req, res) => {
+  res.sendStatus(200)
+  const axios = require('axios')
+  await axios.post('https://asa.team/api/slack/asaSlack', req)
 })
 
 app.listen(port, () => {
