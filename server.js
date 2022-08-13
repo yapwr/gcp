0d851152207d5496bd85cf72037f15facf4a5d99
MarcTzh <marcustan26@hotmail.com>
2022-08-13 17:00:57 +0800

    add req in post request

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v1.1')
+  res.send('v1.2')
 })
 
 app.post('/slack', async (req, res) => {
   // const start = new Date().getTime()
   // functions.logger.log('Start of slack')
   const axios = require('axios')
-  await axios.post('https://asa.team/api/slack/ping')
+  await axios.post('https://asa.team/api/slack/ping', req)
   res.sendStatus(200)
   // functions.logger.log('After res send 200', new Date().getTime() - start)
   // functions.logger.log('End of slack', new Date().getTime() - start)
